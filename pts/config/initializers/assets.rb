66d1815f6b2190a29f700446003470afca8760fb
slhcnzl <can.ozel@bil.omu.edu.tr>
2016-05-16 04:16:55 +0300

    #3 Login ekranı tasarlandı

--- pts/config/initializers/assets.rb
+++ pts/config/initializers/assets.rb
@@ -9,5 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( animate.min.css pts.css bootstrap.css font-awesome.min.css)
-Rails.application.config.assets.precompile += %w( application.js)
+Rails.application.config.assets.precompile += %w( jquery.js bootstrap.css )
